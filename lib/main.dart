ad0a91d089fb172376f358875c44d138345d7bc4
shota-nukumizu <author2000.1225@gmail.com>
2022-07-16 11:13:06 +0900

    create main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,62 @@
-import 'package:flutter/material.dart';
+import 'package:opensea_dart/enums/enums.dart';
+import 'package:opensea_dart/opensea_dart.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  String? apiKey = "Put you api key here, or pass null to use without apikey";
+  final openSea = OpenSea(apiKey);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  ///getOrders (requires apiKey to be set)
+  openSea
+      .getOrders(side: OrderSide.buy, limit: "1")
+      .then((value) => print(value));
+  await Future.delayed(const Duration(seconds: 5));
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  ///getEvents (requires apiKey to be set)
+  openSea.getEvents(limit: "1").then((value) => print(value));
+  await Future.delayed(const Duration(seconds: 5));
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  ///getCollectionStats
+  openSea
+      .getCollectionStats(slug: "copypasteearth")
+      .then((value) => print(value));
+  await Future.delayed(const Duration(seconds: 5));
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  ///getContract
+  openSea
+      .getContract(
+          assetContractAddress: "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb")
+      .then((value) => print(value));
+  await Future.delayed(const Duration(seconds: 5));
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  ///getAsset
+  openSea
+      .getAsset(
+          assetContractAddress: "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
+          tokenId: "1",
+          accountAddress: "0xb88f61e6fbda83fbfffabe364112137480398018")
+      .then((value) => print(value));
+  await Future.delayed(const Duration(seconds: 5));
 
-  final String title;
+  ///getBundles
+  openSea.getBundles(
+      tokenIds: ["1", "209"], limit: "1").then((value) => print(value));
+  await Future.delayed(const Duration(seconds: 5));
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  ///getCollections
+  openSea.getCollections(limit: "1", offset: "5").then((value) => print(value));
+  await Future.delayed(const Duration(seconds: 5));
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  ///getCollection
+  openSea.getCollection("copypasteearth").then((value) => print(value));
+  await Future.delayed(const Duration(seconds: 5));
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  ///getAssets
+  openSea
+      .getAssets(
+          orderBy: OrderBy.saleDate,
+          orderDirection: OrderDirection.asc,
+          limit: "1",
+          offset: "0",
+          collection: "doodles-official")
+      .then((value) => print(value));
 }
